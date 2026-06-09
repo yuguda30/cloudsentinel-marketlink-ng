85f08aa88fb0085546e96b49f7716061d5d82ec3
Muhammad Yuguda <muhammadyuguda32@gmail.com>
2026-06-09 22:20:42 +0100

    update

--- config.py
+++ config.py
@@ -4,24 +4,34 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    if os.environ.get("RENDER"):
-        db_path = "/tmp/marketlink.db"
-    else:
-        db_path = os.path.join(basedir, "marketlink.db")
 
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
+    # Flask Secret Key
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "cloudsentinel-marketlink-secret-2026"
+    )
+
+    # SQLite Database
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" +
+        os.path.join(BASE_DIR, "instance", "marketlink.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "uploads", "listings")
+    # Upload Settings
+    UPLOAD_FOLDER = os.path.join(
+        BASE_DIR,
+        "static",
+        "uploads",
+        "listings"
+    )
 
-    ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png"}
+    ALLOWED_IMAGE_EXTENSIONS = {
+        "jpg",
+        "jpeg",
+        "png"
+    }
 
-    MAX_IMAGE_SIZE = 1 * 1024 * 1024  # 1MB only
-
-database_url = os.environ.get("DATABASE_URL")
-
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
-
-SQLALCHEMY_DATABASE_URI = database_url or "sqlite:///" + os.path.join(BASE_DIR, "instance", "marketlink.db")
+    # Maximum Image Size = 1MB
+    MAX_IMAGE_SIZE = 1 * 1024 * 1024
